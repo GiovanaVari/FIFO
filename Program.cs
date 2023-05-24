151631e88f4a99119a8510d1e5f0c4c982a32bfd
GiovanaVari <giovana.vari.de.assis@gmail.com>
2023-05-24 18:11:31 -0300

    FIFO

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(8);
+fila.Enqueue(5);
+fila.Enqueue(72);
+fila.Enqueue(88);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+fila.Enqueue(1);
+
+foreach(int item in fila)
+{
+    Console.WriteLine(item);
+}
